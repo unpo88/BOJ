55d9d8e3674fd29e725ad77259fc9ffe211300bc
unpo88 <whatsup@khu.ac.kr>
2018-03-19 11:31:01 +0900

    [Modified] SW_Expert 1206 Problem - Add Comment

--- BaekJoon/180319/1206.cpp
+++ BaekJoon/180319/1206.cpp
@@ -15,21 +15,23 @@ int main() {
 		for (int j = 0; j < tc_number; j++) {
 			cin >> map[j];
 		}
-		for (int j = 2; j < tc_number - 2; j++) {	// 조망권 확보
-			int a = map[j - 2];
-			int b = map[j - 1];
-			int c = map[j + 1];
-			int d = map[j + 2];
-			int center = map[j];
-			int m = max(a, max(b, max(c, d)));
+		for (int j = 2; j < tc_number - 2; j++) {	// 맨 왼쪽과 오른쪽 두 칸에는 건물이 지어지지 않는다.
+													// 체크해야하는 건물	(조망권 체크)(양쪽으로 거리 2 이상의 공간이 확보)
+			int m_left2 = map[j - 2];
+			int m_left1 = map[j - 1];
+			int m_right1 = map[j + 1];
+			int m_right2 = map[j + 2];
+			int center = map[j];	// 중심 건물 center
+			int m = max(m_left2, max(m_left1, max(m_right1, m_right2)));	// 가장 높은 건물
 			if (center > m) {
-				ans = ans + (center - m);
+				ans = ans + (center - m);	// 조망권 빌딩
 			}
 		}
 		cout << "#" << i << " " << ans << endl;
 	}
 }
 
+// MAX 함수
 int max(int a, int b) {
 	if (a > b) {
 		return a;
