7ece80e8d1030b207b48c02a909b664521a83ab1
unpo88 <whatsup@khu.ac.kr>
2018-03-28 08:39:48 +0900

    [Update] BOJ 1003 Problem - Comment Add

--- BaekJoon/180323/1003.cpp
+++ BaekJoon/180323/1003.cpp
@@ -3,19 +3,20 @@
 int N;	// Number
 int T;	// TestCase
 
-int dp_zero[41];
-int dp_one[41];
+int dp_zero[41];	// 규칙을 넣을 배열
+int dp_one[41];		// 규칙을 넣을 배열
 
 int main() {
-	scanf("%d", &T);		// TestCase\
+	scanf("%d", &T);		// TestCase
 
+	// 초기값
 	dp_zero[0] = 1;
 	dp_zero[1] = 0;
 
 	dp_one[0] = 0;
 	dp_one[1] = 1;
 
-	for (int i = 2; i < 41; i++) {
+	for (int i = 2; i < 41; i++) {	// 범위는 40보다 작거나 같은 자연수 또는 0
 		dp_zero[i] = dp_zero[i - 1] + dp_zero[i - 2];
 		dp_one[i] = dp_one[i - 1] + dp_one[i - 2];
 	}
