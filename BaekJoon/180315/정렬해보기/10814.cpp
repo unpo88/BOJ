f05c7902ccc2ddac7cd8dffd622b4658a9c1d89a
최원섭 <whatsup@khu.ac.kr>
2018-04-14 13:42:51 +0900

    [Update] BOJ

---
+++ BaekJoon/180315/정렬해보기/10814.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+struct Person {
+	string name;
+	int age;
+	int join;
+};
+
+bool cmp(const Person &u, const Person &v) {
+	if (u.age < v.age) {
+		return true;
+	}
+	else if (u.age == v.age) {
+		if (u.join < v.join) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	vector<Person> v1(N);
+
+	int cnt = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> v1[i].age >> v1[i].name;
+		v1[i].join = ++cnt;
+	}
+
+	sort(v1.begin(), v1.end(), cmp);
+
+	for (int i = 0; i < N; i++) {
+		cout << v1[i].age << ' ' << v1[i].name << '\n';
+	}
+
+	return 0;
+}
