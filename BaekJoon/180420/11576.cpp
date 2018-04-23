44bfc217eb76c5527deaac8f0a4577cfb9db59d7
Sub <whatsup@khu.ac.kr>
2018-04-23 15:16:06 +0900

    [Update] BOJ Problem 11576

--- BaekJoon/180420/11576.cpp
+++ BaekJoon/180420/11576.cpp
@@ -1,15 +1,36 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
-int arr[30];
+int arr[100];
 int main() {
 	int A, B;
 	cin >> A >> B;
-	int N;
-	int num;
-	while (N--) {
-		cin >> num;
 
+	int N;
+	cin >> N;
+
+	int num;
+	int sum = 0;
+	while (N) {
+		cin >> num;
+		N = N-1;
+		sum = sum + num * pow(A,N);
 	}
+
+	int cnt = 0;
+
+	while(sum >= B){
+		arr[cnt] = sum % B;
+		cnt = cnt+1;
+		sum = sum / B;
+	}
+	
+	arr[cnt] = sum;
+	
+	for(int i = cnt; i >= 0; i--){
+		cout << arr[i] << " ";
+	}
+
 	return 0;
 }
