a21d5d8364b28c513e0a0fefe2d2faceb1a3c8dc
최원섭 <whatsup@khu.ac.kr>
2018-04-23 10:00:20 +0900

    [Update] BOJ Problem 1168 Modified

--- BaekJoon/180420/1168.cpp
+++ BaekJoon/180420/1168.cpp
@@ -1,56 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-int tree[100001];
-void update(int idx, int val) {
-	while (idx <= 100000) {
-		tree[idx] += val;
-		idx += idx&(-idx);
-	}
-}
-int query(int idx) {
-	int ret = 0;
-	while (idx>0) {
-		ret += tree[idx];
-		idx -= idx&(-idx);
-	}
-	return ret;
-}
-int n, m;
-bool possi(int x, int st, int k, int cnt) {
-	if (x>st)
-		return query(x) - query(st) >= k;
-	return cnt - query(st) + query(x) >= k;
-}
+
 int main() {
-	scanf("%d%d", &n, &m);
-	for (int i = 1; i <= n; i++)
-		update(i, 1);
-	int now = 0;
-	printf("<");
-	while (true) {
-		int cnt = query(n);
-		int k = (m - 1) % cnt + 1;
-		int le, ri, mid;
-		le = 1; ri = n;
-		int next;
-		while (le <= ri) {
-			mid = (le + ri) / 2;
-			int xmid = (now + mid - 1) % n + 1;
-			if (possi(xmid, now, k, cnt)) {
-				next = xmid;
-				ri = mid - 1;
-			}
-			else
-				le = mid + 1;
-		}
-		printf("%d", next);
-		update(next, -1);
-		now = next;
-		if (cnt>1)
-			printf(", ");
-		else
-			break;
+	int N, M;
+	cin >> N >> M;
+
+	vector<int> v(N);
+
+	for (int i = 0; i < N; i++) {
+		v[i] = i + 1;
 	}
-	printf(">\n");
+
+	int pos = 0;
+
+	cout << "<";
+	while (v.size() != 1) {
+		pos += M - 1;
+
+		if (pos >= v.size()) {
+			pos %= v.size();
+		}
+
+		cout << v[pos] << ", ";
+		v.erase(v.begin() + pos);
+	}
+
+	cout << v[0];
+	cout << ">";
+
 	return 0;
 }
