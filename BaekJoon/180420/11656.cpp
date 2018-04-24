779cab5d3ddd84a4e6f30d46883ebea12f62970b
Sub <whatsup@khu.ac.kr>
2018-04-24 11:21:10 +0900

    [Update] BOJ Problem 11656

---
+++ BaekJoon/180420/11656.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    string s;
+    cin >> s;
+    
+    vector<string> v(s.size());
+
+    for(int i = 0; i < s.size(); i++){
+        v[i] = s.substr(i);
+    }
+
+    sort(v.begin(), v.end());
+
+    for(auto x : v){
+        cout << x << '\n';
+    }
+
+    return 0;
+}
