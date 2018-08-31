787917084df822bc3809a37c4e76b40a2145ad7d
Sub <whatsup@khu.ac.kr>
2018-08-31 13:18:01 +0900

    [Modified] TopCoder Problem BadNeighbors is Optimization

--- BaekJoon/TOPCODER/BadNeighbors.cpp
+++ BaekJoon/TOPCODER/BadNeighbors.cpp
@@ -3,8 +3,7 @@
 using namespace std;
 
 int donations[41];
-int check[41];
-int check2[41];
+int dp[41];
 
 int N;
 
@@ -15,39 +14,28 @@ int main(){
 
     for(int i = 1; i <= N; i++){
         scanf("%d", &donations[i]);
-        check[i] = check2[i] = donations[i];
     }
 
-    for(int i = 1; i <= 2; i++){
-        for(int j = 1; j <= N; j++){
-            if(i == 1){
-                if(j == N){
-                    check[j] = 0;
-                    continue;
-                }else{
-                    if(j == 1 || j == 2){
-                        answer = max(answer, check[j]);
-                        continue;
-                    }else{
-                        check[j] = max((check[j] + check[j-3]), (check[j] + check[j-2]));
-                        answer = max(answer, check[j]);
-                    }
-                }
-            }else if(i == 2){
-                if(j == 1){
-                    check2[j] = 0;
-                    continue;
-                }else{
-                    if(j == 2 || j == 3){
-                        answer = max(answer, check2[j]);
-                        continue;
-                    }else{
-                        check2[j] = max((check2[j] + check2[j-3]), (check2[j] + check2[j-2]));
-                        answer = max(answer,check2[j]);
-                    }
-                }
-            }
+    for(int i = 1; i <= N - 1; i++){
+        dp[i] = donations[i];
+        if(i > 1){
+            dp[i] = max(dp[i], dp[i-1]);
         }
+        if(i > 2){
+            dp[i] = max(dp[i], dp[i-2] + donations[i]);
+        }
+        answer = max(answer, dp[i]);
+    }
+
+    for(int i = 1; i <= N - 1; i++){
+        dp[i] = donations[i+1];
+        if(i > 1){
+            dp[i] = max(dp[i], dp[i-1]);
+        }
+        if(i > 2){
+            dp[i] = max(dp[i], dp[i-2] + donations[i+1]);
+        }
+        answer = max(answer, dp[i]);
     }
 
     printf("%d\n", answer);
