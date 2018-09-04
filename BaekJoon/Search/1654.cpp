02d175ec648b06024028e18f95b4513e1334cb59
Sub <whatsup@khu.ac.kr>
2018-09-04 21:07:09 +0900

    [Update] BOJ Problem 1654 Solved

---
+++ BaekJoon/Search/1654.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+#include <climits>
+
+int N, K;
+
+long long lan[10001];
+long long max = 0;
+long long left = 0;
+long long right = LLONG_MAX;
+
+int cnt = 0;
+int cnt2 = 0;
+int main(){
+    scanf("%d %d", &K, &N);
+
+    for(int i = 0; i < K; i++){
+        scanf("%lld", &lan[i]);
+    }
+
+    while(left <= right){
+        long long mid = (left + right) / 2;
+        int result = 0;
+        for(int i = 0; i < K; i++){
+            result += lan[i] / mid;
+        }
+
+        if(result >= N){
+            left = mid + 1;
+            if(mid > max){
+                max = mid;
+            }
+        }else{
+            right = mid - 1;
+        }
+    }
+
+    printf("%lld\n", max);
+
+    return 0;
+}
