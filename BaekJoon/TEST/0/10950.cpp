7f4e6fbc95ac377137bb37a3655b1f59494aa214
Sub <whatsup@khu.ac.kr>
2018-09-06 11:41:16 +0900

    [0] BOJ Problem 10950 Solved

---
+++ BaekJoon/TEST/0/10950.cpp
@@ -0,0 +1,14 @@
+#include <cstdio>
+
+int main(){
+    int T;
+    scanf("%d", &T);
+    int a, b;
+    
+    while(T--){
+        scanf("%d %d", &a, &b);
+        printf("%d\n", a+b);
+    }
+
+    return 0;
+}
