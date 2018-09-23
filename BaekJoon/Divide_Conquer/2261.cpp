02de4a79872221bebeb1f917718df869e4fd34b6
Sub <whatsup@khu.ac.kr>
2018-09-23 19:30:36 +0900

    [Update] BOJ Problem 2261 Solved[Difficult]

---
+++ BaekJoon/Divide_Conquer/2261.cpp
@@ -0,0 +1,95 @@
+#include <cstdio>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int N;
+
+struct Point{
+    int x, y;
+    Point(){
+
+    }
+    Point(int x, int y) : x(x), y(y){
+    }
+    bool operator < (const Point &v) const{
+        if(x == v.x){
+            return y < v.y;
+        }else{
+            return x < v.x;
+        }
+    }
+};
+
+int dist(Point p1, Point p2){
+    return (p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y);
+}
+
+int brute_force(vector<Point> &a, int x, int y){
+    int ans = -1;
+    for(int i = x; i <= y - 1; i++){
+        for(int j = i + 1; j <= y; j++){
+            int d = dist(a[i], a[j]);
+            if(ans == -1 || ans > d){
+                ans = d;
+            }
+        }
+    }
+    return ans;
+}
+
+bool cmp(const Point &u, const Point &v){
+    return u.y < v.y;
+}
+
+int closet(vector<Point> &a, int x, int y){
+    int n = y - x + 1;
+    if(n <= 4){
+        return brute_force(a, x, y);
+    }
+    int mid = (x + y) / 2;
+    int left = closet(a, x, mid);
+    int right = closet(a, mid + 1, y);
+    int ans = min(left, right);
+    vector<Point> b;
+
+    for(int i = x; i <= y; i++){
+        int d = a[i].x - a[mid].x;
+        if(d * d < ans){
+            b.push_back(a[i]);
+        }
+    }
+
+    sort(b.begin(), b.end(), cmp);
+
+    int m = b.size();
+    for(int i = 0; i < m - 1; i++){
+        for(int j = i + 1; j < m; j++){
+            int y = b[j].y - b[i].y;
+            if(y * y < ans){
+                int d = dist(b[i], b[j]);
+                if(d < ans){
+                    ans = d;
+                }else{
+                    break;
+                }
+            }
+        }
+    }
+    return ans;
+}
+
+int main(){
+    scanf("%d", &N);
+    vector<Point> a(N);
+
+    for(int i = 0; i < N; i++){
+        scanf("%d %d", &a[i].x, &a[i].y);
+    }
+
+    sort(a.begin(), a.end());
+
+    printf("%d\n", closet(a, 0, N - 1));
+
+    return 0;
+}
