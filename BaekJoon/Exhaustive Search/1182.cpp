d359d64d3e25828c072fc4551c51b910375fccc2
Sub <whatsup@khu.ac.kr>
2018-09-28 22:11:06 +0900

    [Update] BOJ Problem 1181 Solved[Difficult]

---
+++ BaekJoon/Exhaustive Search/1182.cpp
@@ -0,0 +1,37 @@
+#include <cstdio>
+
+int num[20];
+
+int N;
+int S;
+
+int ans;
+
+void cal(int index, int sum){
+    sum += num[index];
+
+    if(index >= N){
+        return ;
+    }
+
+    if(sum == S){
+        ans++;
+        return ;
+    }
+
+    cal(index + 1, sum - num[index]);
+    cal(index + 1, sum);
+}
+
+int main(){
+    scanf("%d %d", &N, &S);
+
+    for(int i = 0; i < N; i++){
+        scanf("%d", &num[i]);
+    }
+
+    cal(0, 0);
+
+    printf("%d\n", ans);
+    return 0;
+}
