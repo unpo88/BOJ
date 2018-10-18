6e694d49fca58dd1d67bbf12c652eca9e3afc735
Sub <whatsup@khu.ac.kr>
2018-10-18 18:24:07 +0900

    [Modified] SAMSUNG SW Expert Clear

--- BaekJoon/SWExpert/15686.cpp
+++ BaekJoon/SWExpert/15686.cpp
@@ -1,74 +1,61 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
-#include <cmath>
 #include <utility>
+#include <algorithm>
 using namespace std;
 
-#define MAX_INF 987654321
-#define MAX_N 51
-#define MAX_M 14
-
 int N, M;
-int ans = MAX_INF;
-
-int map[MAX_N][MAX_N];
-bool check[MAX_M];
 
 vector<pair<int, int> > house;
-vector<pair<int, int> > chiken;
+vector<pair<int, int> > chicken;
 
-void go(int cur_num, int cur_cnt){
-    if(cur_num > chiken.size())    return ;
+int map[51][51];
+bool check[14];
 
-    if(cur_cnt == M){
-        int cmp = 0;
+int answer = 987654321;
+
+void DFS(int idx, int cnt){
+    if(idx > chicken.size())    return ;
+
+    if(cnt == M){
+        int sum = 0;
         for(int i = 0; i < house.size(); i++){
-            int dist = MAX_INF;
-            for(int j = 0; j < chiken.size(); j++){
+            int dist = 987654321;
+            for(int j = 0; j < chicken.size(); j++){
                 if(check[j]){
-                    int px = house[i].first;
-                    int py = house[i].second;
-                    int nx = chiken[j].first;
-                    int ny = chiken[j].second;
-                    int d = abs(px - nx) + abs(py - ny);
+                    int d = abs(house[i].first - chicken[j].first) + abs(house[i].second - chicken[j].second);
                     dist = min(dist, d);
                 }
             }
-            cmp += dist;
+            sum += dist;
         }
-        ans = min(ans,cmp);
+        answer = min(answer, sum);
         return ;
     }
 
-    check[cur_num] = true;
-    go(cur_num + 1, cur_cnt + 1);
-    check[cur_num] = false;
-    go(cur_num + 1, cur_cnt);
-
+    check[idx] = true;
+    DFS(idx + 1, cnt + 1);
+    check[idx] = false;
+    DFS(idx + 1, cnt);
 }
 
 int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
     cin >> N >> M;
-    for(int i = 0; i < N; i++){
-        for(int j = 0; j < N; j++){
+
+    for(int i = 1; i <= N; i++){
+        for(int j = 1; j <= N; j++){
             cin >> map[i][j];
-            if(map[i][j] == 1){
+            if(map[i][j] == 2){
+                chicken.push_back(make_pair(i, j));
+            }else if(map[i][j] == 1){
                 house.push_back(make_pair(i, j));
-            }else if(map[i][j] == 2){
-                chiken.push_back(make_pair(i, j));
             }
         }
     }
 
-    go(0, 0);
+    DFS(0, 0);
 
-    cout << ans << endl;
-
+    cout << answer << '\n';
 
     return 0;
 }
