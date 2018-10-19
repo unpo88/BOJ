b930c9060ebd9b1afc98d39f3d40b132f45c9ba8
Sub <whatsup@khu.ac.kr>
2018-10-19 21:11:54 +0900

    Samsung SW Clear

--- BaekJoon/SWExpert/14891.cpp
+++ BaekJoon/SWExpert/14891.cpp
@@ -74,3 +74,96 @@ int main(){
     cout << ans << '\n';
     return 0;
 }
+
+/*
+
+// 8시 20분 -> 9시 10분
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int T[5][9];
+int K;
+
+int d[5];
+
+int main(){
+    for(int i = 1; i <= 4; i++){
+       string str = "";
+       cin >> str;
+        for(int j = 1; j <= 8; j++){
+            T[i][j] = str[j-1]-'0';
+        }
+    }
+    
+    cin >> K;
+
+    while(K--){
+        for(int i = 1; i <= 4; i++){
+            d[i] = 0;
+        }
+        int no, dir;
+        cin >> no >> dir;
+        d[no] = dir;
+
+        // 왼쪽
+        int temp_d = dir;
+        for(int i = no - 1; i >= 1; i--){
+            if(T[i][3] != T[i+1][7]){
+                temp_d *= -1;
+                d[i] = temp_d;
+            }else{
+                break;
+            }
+        }
+
+        // 오른쪽
+        temp_d = dir;
+        for(int i = no + 1; i <= 4; i++){
+            if(T[i-1][3] != T[i][7]){
+                temp_d *= -1;
+                d[i] = temp_d;
+            }else{
+                break;
+            }
+        }
+
+        for(int i = 1; i <= 4; i++){
+            if(d[i] == 1){
+                int temp = T[i][1];
+                T[i][1] = T[i][8];
+                T[i][8] = T[i][7];
+                T[i][7] = T[i][6];
+                T[i][6] = T[i][5];
+                T[i][5] = T[i][4];
+                T[i][4] = T[i][3];
+                T[i][3] = T[i][2];
+                T[i][2] = temp;
+
+            }else if(d[i] == -1){
+                int temp = T[i][1];
+                T[i][1] = T[i][2];
+                T[i][2] = T[i][3];
+                T[i][3] = T[i][4];
+                T[i][4] = T[i][5];
+                T[i][5] = T[i][6];
+                T[i][6] = T[i][7];
+                T[i][7] = T[i][8];
+                T[i][8] = temp;
+            }
+        }
+    }
+
+    
+
+    int answer = 0;
+    int mul[5] = {0, 1, 2, 4, 8 };
+    for(int i = 1; i <= 4; i++){
+        if(T[i][1] == 1){
+            answer += mul[i];
+        }   
+    }
+    cout << answer << endl;
+    return 0;
+}*/
