48f7dfff8a27ddb47bbad47ca89e4b903896f24d
Sub <whatsup@khu.ac.kr>
2018-10-22 16:42:09 +0900

    [Update] BOJ 11047 Clear

--- BaekJoon/Greedy/11047.cpp
+++ BaekJoon/Greedy/11047.cpp
@@ -1,27 +1,36 @@
-#include <cstdio>
+#include <iostream>
+#include <vector>
+using namespace std;
 
-int N;
-long long K;
-int A[10];
+int N, K;
 
-int answer;
+int sum;
+vector<int> v;
 
 int main(){
-    scanf("%d", &N);
-    scanf("%lld", &K);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    cin >> N >> K;
+
     for(int i = 0; i < N; i++){
-        scanf("%d", &A[i]);
+        int num;
+        cin >> num;
+        v.push_back(num);
     }
 
-    for(int i = N - 1; i >= 0; i--){
-        if(A[i] <= K){
-            answer += K / A[i];
-            K = K % A[i];
+    for(int i = v.size() - 1; i >= 0; i--){
+        if(v[i] > K)    continue;
+        if(v[i] <= K){
+            int n = 0;
+            n += K / v[i];
+            K -= n * v[i];
+            sum += n;
         }
     }
 
-    printf("%d\n", answer);
-
+    cout << sum << '\n';
 
     return 0;
 }
